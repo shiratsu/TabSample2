bcee48d7283aa52ad7ff9777d4c5de8e3ce04572
平塚俊輔 <shun_hiratsuka2002@yahoo.co.jp>
2015-02-17 18:01:27 +0900

    iroiro update

---
+++ PonyDebugSample/PonyDebugSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PonyDebugSample
+//
+//  Created by HIRATSUKA SHUNSUKE on 2015/02/16.
+//  Copyright (c) 2015年 HIRATSUKA SHUNSUKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PonyDebugSample/PonyDebugSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PonyDebugSample
+//
+//  Created by HIRATSUKA SHUNSUKE on 2015/02/16.
+//  Copyright (c) 2015年 HIRATSUKA SHUNSUKE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
